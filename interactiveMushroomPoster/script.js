ca63363d5a9cd56e93dd64f101038d2c335bf2e4
davAnGo <bluberrycouch@gmail.com>
2022-07-02 23:57:15 -0600

    personal front end project. nice looking display of mushrooms with an interactive twist unveiling flavor profiles of the clicked upon fungus

---
+++ interactiveMushroomPoster/script.js
@@ -0,0 +1,83 @@
+function shadow(element){
+    element.classList.add("shadow");
+}
+function removeShadow(element){
+    element.classList.remove("shadow");
+}
+function returnToTitle(){
+    console.log("title returned")
+    document.getElementById("textBox").innerHTML="Fungal Flavors"
+    document.getElementById("textBox").style.fontSize="24pt";
+}
+
+function showFact0(){
+    console.log("showing champignone fact")
+    document.getElementById("textBox").innerText="The Royal Champignon is an amazing mushroom that is rich in vitamins and minerals. It is not only an excellent nutritious product, but also has many useful properties, thanks to which it is widely used in cosmetology, dietetics and traditional medicine.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact1(){
+    console.log("showing porcini fact")
+    document.getElementById("textBox").innerText="The porcini mushroom is meaty and the taste is intense: rich and woodsy with subtle nutty undertones. These mushrooms are delicate in flavor but vigorous enough in body to be used in brown sauces, and will stand up to strong flavors like grilled steak.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact2(){
+    console.log("showing portabella fact")
+    document.getElementById("textBox").innerText="The smooth cap of the Portabella ranges from dark brown to tan and is firm, thick, and spongy. Underneath the cap, there are dark brown, fleshy gills, a small ring from the cotton-like veil, and the stem is fibrous, white, and dense. When cooked, Portabella mushrooms have a memorable chewy and meaty texture and a smoky, earthy flavor.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact3(){
+    console.log("showing enoki fact")
+    document.getElementById("textBox").innerText="The snow-white caps of the Enoki mushrooms are tender, smooth, delicate, and are connected to matching white stems that are thin and can grow up to twelve centimeters in length. Cultivated Enoki mushrooms are crisp and firm with a crunchy bite and a mild, fruity flavor.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact4(){
+    console.log("showing lobster fact")
+    document.getElementById("textBox").innerText="Lobster mushroom is meaty and has a dense texture with a nutty, sweet smell reminiscent of steamed lobster. The lobster mushroom is found in the autumn in New England and on the West Coast, usually from late summer through October, under a variety of trees, most often hemlock.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact5(){
+    console.log("showing shimeji fact")
+    document.getElementById("textBox").innerText="Hon Shimeji mushrooms have a savory, nutty, umami flavor, with slightly sweet and buttery notes. The brown variety are a bit richer, while the white ones are milder, but both types are loaded with mushroomy flavor.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact6(){
+    console.log("showing chicken of the woods  fact")
+    document.getElementById("textBox").innerText="Chicken of the Woods mushrooms are medium to large in size, When cooked, Chicken of the Woods mushrooms are juicy, succulent, and meaty with a mild, lemony flavor that many compare to the taste of chicken, lobster, or crab";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact7(){
+    console.log("showing morel fact")
+    document.getElementById("textBox").innerText="Unlike many cultivated mushrooms such as cremini and portabella that have a robust, meaty flavor when cooked, MORELS have a much more subtle texture and taste. They are often described as earthy, woodsy, and nutty. The darker varieties can even have a mild smokiness.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact8(){
+    console.log("showing suillus fact")
+    document.getElementById("textBox").innerText="The fragrant bolete, Suillus punctipes (Peck) Singer, is an edible mushroom with a unique aroma reminiscent of mushroom and citrus peel with an undertone of apricot.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact9(){
+    console.log("showing chanterelle fact")
+    document.getElementById("textBox").innerText="The delicate Chanterelle mushrooms have a peppery flavor that some people describe as lightly fruity. The best part about them is the aroma, which is almost apricot-like. As you cook them, they soften and create a melt-in-your-mouth texture.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact10(){
+    console.log("showing honey agaric fact")
+    document.getElementById("textBox").innerText="Yes, the Honey Agaric are edible. They have, as you can imagine, a slightly sweet taste along with a chewy, first texture.Yet despite the appealing name, these mushrooms can also be slightly bitter and are known to cause some gastric distress. That’s why many people recommend parboiling them for one or two minutes before cooking.";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
+
+function showFact11(){
+    console.log("showing black truffle fact")
+    document.getElementById("textBox").innerText="Some may describe the taste of black truffles as a mixture of chocolaty, nutty and earthy and others may describe it as having a subtle woody flavor mixed with a slight mushroom taste. The region where a truffle is harvested from plays a large role in the taste";
+    document.getElementById("textBox").style.fontSize="10pt";
+}
